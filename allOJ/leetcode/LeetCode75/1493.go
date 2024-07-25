a39869b838e34a5c2971a8f88a5a2fcf737f1fd6
Strork <strork@qq.com>
2024-07-25 19:00:39 +0800

    leetcode75

---
+++ allOJ/leetcode/LeetCode75/1493.go
@@ -0,0 +1,21 @@
+func longestSubarray(nums []int) int {
+	var x = [2]int{-1, -1}
+	var y = [2]int{-1, -1}
+	ans := 0
+	ff := false
+	// 1 1 0 1
+	for i, v := range nums {
+		if v == 0 {
+			ff = true
+			ans = max(ans, y[1]-x[1]+y[0]-x[0])
+			x[0], y[0] = x[1], y[1]
+			x[1] = i
+		}
+		y[1] = i
+	}
+	ans = max(ans, y[1]-x[1]+y[0]-x[0])
+	if !ff {
+		ans--
+	}
+	return ans
+}
---
+++ allOJ/leetcode/LeetCode75/452.go
@@ -0,0 +1,15 @@
+func findMinArrowShots(points [][]int) int {
+	sort.Slice(points, func(i, j int) bool {
+		return points[i][1] < points[j][1]
+	})
+	//fmt.Println(points)
+	ans := 1
+	y := points[0][1]
+	for _, v := range points {
+		if v[0] > y {
+			y = v[1]
+			ans++
+		}
+	}
+	return ans
+}
