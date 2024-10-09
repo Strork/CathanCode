0828b3e919abd7ff04b54154d12c3a229bd29f6f
Strork <strork@qq.com>
2024-10-09 17:33:06 +0800

    20241009

---
+++ allOJ/leetcode/2286.booking-concert-tickets-in-groups.cpp
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=2286 lang=cpp
+ * @lcpr version=30204
+ *
+ * [2286] 以组为单位订音乐会的门票
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class BookMyShow {
+public:
+    BookMyShow(int n, int m) {
+
+    }
+    
+    vector<int> gather(int k, int maxRow) {
+
+    }
+    
+    bool scatter(int k, int maxRow) {
+
+    }
+};
+
+/**
+ * Your BookMyShow object will be instantiated and called as such:
+ * BookMyShow* obj = new BookMyShow(n, m);
+ * vector<int> param_1 = obj->gather(k,maxRow);
+ * bool param_2 = obj->scatter(k,maxRow);
+ */
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// ["BookMyShow", "gather", "gather", "scatter", "scatter"][[2, 5], [4, 0], [2, 0], [5, 1], [5, 1]]\n
+// @lcpr case=end
+
+ */
+
